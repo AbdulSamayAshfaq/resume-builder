9457706c775dc8fdd48ec7df5da07cb2f9f49401
iTx_samay <160161407+AbdulSamayAshfaq@users.noreply.github.com>
2024-11-05 20:23:23 +0500

    portfolio resume
    resume

--- script.js
+++ script.js
@@ -1,23 +1,10 @@
-// get reference to the from and display area
-var from = document.getElementById('resume-from');
-var resumeDisplayElement = document.getElementById('resume-display');
-// handle form submission 
-from.addEventListener('submit', function (event) {
-    event.preventDefault(); // prevent page reload
-    //collect input value
-    var name = document.getElementById('name').value;
-    var email = document.getElementById('email').value;
-    var phone = document.getElementById('phone').value;
-    var education = document.getElementById('education ').value;
-    var experience = document.getElementById('experience').value;
-    var skills = document.getElementById('skills').value;
-    // generate the resume content dynamically
-    var resumeHTML = "\n    \n     <h2><b>Editableresume</b></h2>\n     <h3>personal Information</h3>\n     <p><b>Name:</b><span contenteditable=\"true\">$(name)</span></p>\n     <p><b>Email:</b><span contenteditable=\"true\">$(email)</span></p>\n     <p><b>Phone:</b><span contenteditable=\"true\">$(phone)</span></p>\n\n     <h3>Education</h3>\n     <p contenteditable=\"true\">$(education)</p>\n\n     <h3>experience</h3>\n     <p contenteditable=\"true\">$(experience)</p>\n\n     <h3>skills</h3>\n    <p contenteditable=\"true\">$(skills)</p>;\n    \n    ";
-    // dispaly the generate resume
-    if (resumeDisplayElement) {
-        resumeDisplayElement.innerHTML = resumeHTML;
+var togglebutton = document.getElementById('toggle-skills');
+var skills = document.getElementById('skills');
+togglebutton.addEventListener('click', function () {
+    if (skills.style.display === 'none') {
+        skills.style.display = 'block';
     }
     else {
-        console.error('the resume display element is missing.');
+        skills.style.display = 'none';
     }
 });
--- script.ts
+++ script.ts
@@ -1,56 +1,10 @@
-// get reference to the from and display area
+const togglebutton = document.getElementById('toggle-skills') as HTMLButtonElement;
+const skills = document.getElementById('skills') as HTMLElement
 
-const from = document.getElementById('resume-from') as HTMLFormElement;
-const resumeDisplayElement = document.getElementById('resume-display') as HTMLDivElement;
-
-// handle form submission 
-
-from.addEventListener('submit' , (event: Event) => {
-    event.preventDefault(); // prevent page reload
-
-
-    //collect input value
-    const name = (document.getElementById('name') as HTMLInputElement).value
-    const email = (document.getElementById('email') as HTMLInputElement).value
-    const phone = (document.getElementById('phone') as HTMLInputElement).value
-    const education = (document.getElementById('education ') as HTMLInputElement).value
-    const experience = (document.getElementById('experience') as HTMLInputElement).value
-    const skills = (document.getElementById('skills') as HTMLInputElement).value
-    
-    // generate the resume content dynamically
-
-    const resumeHTML = `
-    
-     <h2><b>Editableresume</b></h2>
-     <h3>personal Information</h3>
-     <p><b>Name:</b><span contenteditable="true">$(name)</span></p>
-     <p><b>Email:</b><span contenteditable="true">$(email)</span></p>
-     <p><b>Phone:</b><span contenteditable="true">$(phone)</span></p>
-
-     <h3>Education</h3>
-     <p contenteditable="true">$(education)</p>
-
-     <h3>experience</h3>
-     <p contenteditable="true">$(experience)</p>
-
-     <h3>skills</h3>
-    <p contenteditable="true">$(skills)</p>;
-    
-    `;
-      
-    
-     
-     
-    
- 
-
-    // dispaly the generate resume
-
-    if(resumeDisplayElement){
-        resumeDisplayElement.innerHTML = resumeHTML;
+togglebutton.addEventListener('click', () => {
+    if(skills.style.display === 'none') {
+        skills.style.display = 'block'
     } else {
-        console.error('the resume display element is missing.');
+        skills.style.display = 'none'
     }
-
-
 })
